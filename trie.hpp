2b0325deaa7231ada4c32abba0fe3711c1814a4a
Jarrod Sanders <thecaptainglaceon@gmail.com>
2020-12-03 21:24:42 -0500

    savepoint

--- trie.hpp
+++ trie.hpp
@@ -1,18 +1,89 @@
 #pragma once
-#include <vector>
-#include <string>
+#include <bits/stdc++.h> 
+
+
+
+// C++ implementation of search and insert 
+// operations on Trie 
+using namespace std; 
+  
+const int ALPHABET_SIZE = 26; 
+  
+// trie node 
+struct TrieNode 
+{ 
+    struct TrieNode *children[ALPHABET_SIZE]; 
+  
+    // isEndOfWord is true if the node represents 
+    // end of a word 
+    bool isEndOfWord; 
+}; 
+  
+// Returns new trie node (initialized to NULLs) 
+struct TrieNode *getNode(void) 
+{ 
+    struct TrieNode *pNode =  new TrieNode; 
+  
+    pNode->isEndOfWord = false; 
+  
+    for (int i = 0; i < ALPHABET_SIZE; i++) 
+        pNode->children[i] = NULL; 
+  
+    return pNode; 
+} 
+  
+// If not present, inserts key into trie 
+// If the key is prefix of trie node, just 
+// marks leaf node 
+void insert(struct TrieNode *root, string key) 
+{ 
+    struct TrieNode *pCrawl = root; 
+  
+    for (int i = 0; i < key.length(); i++) 
+    { 
+        int index = key[i] - 'a'; 
+        if (!pCrawl->children[index]) 
+            pCrawl->children[index] = getNode(); 
+  
+        pCrawl = pCrawl->children[index]; 
+    } 
+  
+    // mark last node as leaf 
+    pCrawl->isEndOfWord = true; 
+} 
+  
+// Returns true if key presents in trie, else 
+// false 
+bool search(struct TrieNode *root, string key) 
+{ 
+    struct TrieNode *pCrawl = root; 
+  
+    for (int i = 0; i < key.length(); i++) 
+    { 
+        int index = key[i] - 'a'; 
+        if (!pCrawl->children[index]) 
+            return false; 
+  
+        pCrawl = pCrawl->children[index]; 
+    } 
+  
+    return (pCrawl != NULL && pCrawl->isEndOfWord); 
+} 
 
 class Trie{
-    struct tNode{
-        long lFreq;
-        bool bIsEndpoint;
-        struct tNode* children[26];
-    };
-
-    tNode* root;
-    tNode* getNewNode();
+    TrieNode* root;
+    bool search();
     public:
     Trie();
-    void addWord(std::string* word);
-    float findWord(std::string* word);
+    void insertWord(std::string word);
+    long getFreq(std::string word);
+
 };
+
+Trie::Trie(){
+    root = GetNode()
+}
+
+void Trie::insertWord(std::string word){
+    cout << word << endl;
+}
