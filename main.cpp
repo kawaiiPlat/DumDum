7a1499fc6eed4007fe87228657e059f9beff11e4
Jarrod Sanders <thecaptainglaceon@gmail.com>
2020-12-06 02:17:25 -0500

    Working on loading in user data

--- main.cpp
+++ main.cpp
@@ -6,10 +6,11 @@
 #include <unordered_map>    // data structure
 #include <map>              // data structure
 #include <fstream>          // for files
-#include <iostream>         //
+#include <iostream>         // 
 #include <iomanip>          // for setprecision
 #include <sstream>          // for csv string parsing
 #include <chrono>           // for timing the different data structures
+#include <vector>           // for storing user objects
 
 
 // from https://www.learncpp.com/cpp-tutorial/8-16-timing-your-code/
@@ -43,20 +44,20 @@ using namespace std;
 
 enum class opType{ single = 0, followers = 1};
 
-struct user{
+struct User{
     string sName;
     float fScore;
-    user(string name, float score){
+    User(string name, float score){
         sName = name;
         fScore = score;
     }
-    bool operator<(user LHS){
+    bool operator<(User LHS){
         if(LHS.fScore > fScore)
             return true;
 
         return false;
     }
-    bool operator>(user LHS){
+    bool operator>(User LHS){
         if(LHS.fScore > fScore)
             return false;
 
@@ -64,7 +65,42 @@ struct user{
     }
 };
 
-void updateTwitter(string name, int mode){
+void parseUserDataInTo(vector<User>& users){
+    ifstream data;
+    string line;
+    data.open("text_files/tweet_data.txt");
+    if (data.is_open()){
+        getline(data, line); // get the mode line
+        if (line.at(0) == 'E'){
+            cerr << "User inaccessable" << endl;
+        } else {
+            getline(data,line); // number of users
+            int iNumUsers = stoi(line);
+
+            // parse each user's data into a new user object
+            for(int i = 0; i < iNumUsers; i++){
+                string sName = "";
+                float fScore = 0;
+                getline(data,sName); // username
+
+                getline(data, line); // numberoftweets
+                int iNumTweets = stoi(line);
+
+                for(int j = 0; j < iNumTweets; j++){
+                    
+                }
+
+                users.emplace_back(User(sName,fScore));
+            }
+
+        }
+    } else {
+        cerr << "data file could not be opened" << endl;
+    }
+    
+}
+
+void updateTwitter(string name, int mode, vector<User>& users){
     Timer t3;
     cout << "reading from twitter, please be patient..." << endl;
     std::string test = "python python_scripts/twitterAccessor.py";
@@ -72,10 +108,11 @@ void updateTwitter(string name, int mode){
     //std::string test = "python python_scripts/twitterAccessor.py LinusTech 0 > /dev/null 2>&1";
     system(test.c_str());
     cout << "search time: " << t3.elapsed() << endl << endl;
+
+    parseUserDataInTo(users);
 }
 
 float findAverageOf(string userName, int mode){
-    updateTwitter(userName,mode);
     // todo calculate with the file
     return 0.0f;
 }
@@ -116,21 +153,27 @@ long getWordScore(unordered_map<string,int>& um, map<string,int>& om, float& umT
     return 0;
 }
 
+string parseUsername(){
+   return ""; 
+}
+
 int main(){
     unordered_map<string,int>   um;
     map<string, int>            om;
     loadWordlist(um,om);
     long umTime = 0, omTime = 0;
+    vector<User> userList;
     cout << setprecision(3) << fixed;
-    string badInput = "bad input, enter the number of an option you want";
-    //updateTwitter("LinusTech", (int)opType::single);
+    const string sBadInput = "bad input, enter the number of an option you want";
+    const string sUsernamePrompt = "Enter the twitter username you want to know about, without the '@' ";
+    
     //updateTwitter("LinusTech", (int)opType::followers);
 
     int choice = -1;
     while (choice != 0){
         cout << "---------------------------------------" << endl;
-        cout << "What operation do you want? Enter 0 to exit" << endl;
-        cout << "[1] Get a single account's score\n[2] Get a account's follower's scores" << endl;
+        cout << "What operation do you want?" << endl;
+        cout << "[0] Exit\n[1] Get a single account's score\n[2] Get a account's follower's scores" << endl;
         string input = "";
         cin >> input;
         
@@ -139,7 +182,7 @@ int main(){
         try{
             choice = stoi(input);
         } catch (exception e){
-            cout << badInput << endl;
+            cout << sBadInput << endl;
         }
 
         // if the input is valid, continue, else print err message
@@ -149,12 +192,49 @@ int main(){
         } else if (choice == 1){
             // single user, just print their score and loop
             cout << "single user" << endl;
+            cout << sUsernamePrompt << endl;
+            cin >> input;
+            updateTwitter(input, (int)opType::single, userList);
+
         } else if (choice == 2) {
             // followers scores, give second menu
             cout << "followers" << endl;
+            cout << sUsernamePrompt << endl;
+            cin >> input;
+            updateTwitter(input, (int)opType::followers, userList);
+           
+            //----------------------------------------------------------
+            // multi user menu
+            choice = -1;
+            while (choice != 0){
+                cout << "What operation do you want?" << endl;
+                cout << "[0] Go back\n[1] View top scoring following\n[2] View bottom scoring following\n[3] view average score of following" << endl;
+                string input = "";
+                cin >> input;
+                cout << endl;
+
+                try{
+                    choice = stoi(input);
+                } catch (exception e){
+                    cout << sBadInput << endl;
+                }
+
+                if(choice == 0){
+                    cout << "Going Back... " << endl;
+                } else if (choice == 1){
+                    
+                } else if (choice == 2){
+
+                } else if (choice == 3){
+
+                }
+            }
+            choice = 2; // so the menu will loop around
+
         } else {
             cout << "pick a valid option" << endl;
         }
+
 
         cout << endl ;
 
