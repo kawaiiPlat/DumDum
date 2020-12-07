38595e849d6149b9473f6d80b81d804274629c9c
Jarrod Sanders <thecaptainglaceon@gmail.com>
2020-12-06 22:38:18 -0500

    updated encrypting

--- python_scripts/encryptKeys.sh
+++ python_scripts/encryptKeys.sh
@@ -1,3 +1,3 @@
 #! /bin/zsh
 tar -czf - apiKeys.json | openssl enc -e -aes256 -out apiKeys.json.tar.gz
-rm apiKeys.json
+#rm apiKeys.json
--- python_scripts/unencryptKeys.sh
+++ python_scripts/unencryptKeys.sh
@@ -1,3 +1,3 @@
 #! /bin/zsh
 openssl enc -d -aes256 -in apiKeys.json.tar.gz | tar xz 
-rm apiKeys.json.tar.gz
+#rm apiKeys.json.tar.gz
