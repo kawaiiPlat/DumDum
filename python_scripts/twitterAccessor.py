f269b49e066c1126f6007962f394e730a1050be7
Ranger Chenore <r2chenore@gmail.com>
2020-12-07 20:44:43 -0500

    Improved Python code comments

--- python_scripts/twitterAccessor.py
+++ python_scripts/twitterAccessor.py
@@ -4,6 +4,12 @@ import tweepy as tw
 import re
 import sys
 
+# Flow of code in main():
+# 1) Validate some of the input
+# 2) Setup libraries/get credentials
+# 3) Access Twitter API, sanitize tweet text, then append to a string
+# 4) Write the string to output file (for C++ code to analyze)
+
 def main():
     data_file_name = "../text_files/tweet_data.txt"
 
@@ -13,6 +19,7 @@ def main():
         output_string_to_file(all_text_to_append, data_file_name)
         return
 
+    # sys.argv accesses the command line arguments
     screen_name = sys.argv[1]
     mode_option = sys.argv[2]  # "0" = get score for screen_name, "1" = get score for all of screen_name's followers
     MAX_FOLLOWER_NUM = 100
@@ -31,10 +38,12 @@ def main():
     if (mode_option == "1"):  # get intelligence scores of all user's followers
         all_followers = tw.Cursor(api.followers, screen_name).items(MAX_FOLLOWER_NUM)  # get MAX_FOLLOWER_NUM most recent followers
         num_followers = 0
+        # try/catch statments galore in case a user's account is private or otherwise inaccessible, which throws an error
         try:
             for follower in all_followers:
                 try:
                     user_info_to_append, num_posts = get_all_posts_by_user(api, follower, MAX_TWEET_NUM)
+                    # append username, number of posts, and tweet text in a chosen format
                     all_text_to_append += follower.screen_name + "\n" + str(num_posts) + "\n" + user_info_to_append
                     num_followers += 1
                 except:
@@ -58,8 +67,8 @@ def main():
             output_string_to_file(all_text_to_append, data_file_name)
             return
     else:
-        all_text_to_append = "ERROR: invalid mode"
-        print("ERROR: invalid mode")
+        all_text_to_append = "ERROR: invalid mode, should be '0' or '1'"
+        print("ERROR: invalid mode, should be '0' or '1'")
         output_string_to_file(all_text_to_append, data_file_name)
         return
     
@@ -71,7 +80,7 @@ def remove_url(txt):
     # regular expression from https://www.earthdatascience.org/courses/use-data-open-source-python/intro-to-apis/calculate-tweet-word-frequencies-in-python/
     return " ".join(re.sub("([^0-9A-Za-z \t])|(\w+:\/\/\S+)", "", txt).split())
 
-def find_rate_limiter(api):
+def find_rate_limiter(api):  # in case Twitter limits our access, this will identify it and tell us exactly what's being limited
     rate_info = api.rate_limit_status()
     rate_str = str(rate_info)
     index = rate_str.find("'remaining': 0")
